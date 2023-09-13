d8244b29ea40a6929cd0d913b8ead8a56bd4123e
Timeman1111 <78430095+Timeman1111@users.noreply.github.com>
2023-09-13 16:50:05 +0000

    Update

--- scene.py
+++ scene.py
@@ -1,76 +1,77 @@
-
-
 class Shop:
-    def __init__(self,shop_name,products = []):
-        #Initalize shop class
 
-        self.name = shop_name
+  def __init__(self, shop_name, products=[]):
+    #Initalize shop class
 
-        self.products = products
+    self.name = shop_name
 
-    def add_product(self,product):
-        #Add single product item to shop category if item is a dictonary
-        if type(product) == dict:
+    self.products = products
 
-            self.products.append(product)
+  def add_product(self, product):
+    #Add single product item to shop category if item is a dictonary
+    if type(product) == dict:
 
-            #Else return negative status
-        else:
-            return False
+      self.products.append(product)
 
-        #Return true
-        return True
-    
-    def remove_product(self,product_index):
+      #Else return negative status
+    else:
+      return False
 
-        #Remove item from the product list
-        try:
+    #Return true
+    return True
 
-            del self.products[product_index]
+  def remove_product(self, product_index):
 
-            #If item is not in list raise error and return false
-        except IndexError:
-            return False
-        else:
+    #Remove item from the product list
+    try:
 
-            #Return True otherwise
-            return True
+      del self.products[product_index]
+
+      #If item is not in list raise error and return false
+    except IndexError:
+      return False
+    else:
+
+      #Return True otherwise
+      return True
 
 
 class Scene:
-    def __init__(self,properties={},trace = None):
-        # Initalize Scene and set class variables
-        
-        self.trace = trace
-        self.properties = properties
-        try:
-          self.properties['shops']
-        except:
-          self.properties['shops'] = []
-    def set_location_text(self,text):
-        #Set scene location text
 
-        self.properties["location_setting_text"] = text
+  def __init__(self, properties={}, trace=None):
+    # Initalize Scene and set class variables
 
-         
-        return True
+    self.trace = trace
+    self.properties = properties
+    try:
+      self.properties['shops']
+    except:
+      self.properties['shops'] = []
+
+  def set_location_text(self, text):
+    #Set scene location text
+
+    self.properties["location_setting_text"] = text
+
+    return True
+
+  def get_scene(self):
+
+    # Get location setting from class property variable
+    location_setting = self.properties["location_setting_text"]
+
+    #Get shop data from class property variable and then get the names
+    shops = self.properties["shops"]
+
+    #Get shop names
+    shops_names = [x["name"] for x in shops]
+
+  def add_shop(self, shop):
+
+    #Add shop to scene
+    self.properties["shops"].append(shop)
+
+    return True
 
 
-    def get_scene(self):
-
-        # Get location setting from class property variable
-        location_setting = self.properties["location_settingxw_text"]
-
-        #Get shop data from class property variable and then get the names
-        shops = self.properties["shops"]
-
-        #Get shop names
-        shops_names = [x["name"] for x in shops]
-    def add_shop(self,shop):
-
-        #Add shop to scene
-        self.properties["shops"].append(shop)
-
-        return True
 Library = Scene()
-
